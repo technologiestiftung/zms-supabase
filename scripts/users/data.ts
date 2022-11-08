10f6fcc15b9e676f5a31e9833c43a0b65b2e7efc
ff6347 <hash@ff6347.email>
2022-11-08 12:26:35 +0100

    chore: Add script to generate multiple users [skip ci]

---
+++ scripts/users/data.ts
@@ -0,0 +1,11 @@
+import { faker } from "@faker-js/faker";
+export const data = [
+	{
+		email: faker.internet.email(),
+		password: faker.internet.password(),
+	},
+	{
+		email: faker.internet.email(),
+		password: faker.internet.password(),
+	},
+];
